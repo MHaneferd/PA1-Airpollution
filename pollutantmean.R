2c5f3f6b2e37ccf25df0a2a9709a941a81cc38e5
MHaneferd <mhaneferd@gmail.com>
2016-07-23 01:07:46 +0200

    The function pollutantmean has been created and testet well. It is finally working.

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,20 @@
-pollutantmean <- function(){
+pollutantmean <- function(directory, pollutant, id = 1:332){
   
+  # Create an empty vector used for calculating the mean at the end.
+  pollvector <- numeric()
+  
+  # Loop trough all files in the id parameter
+  for(index in id){
+    # Obtain directory and filename
+    filename = paste(directory,"/", formatC(index, width=3, flag="0"),".csv",sep="")
+    
+    #Read file
+    polldata = read.csv(filename)
+    
+    # Increase the vector with the column of the file that is selected with pollutant param.
+    pollvector <- rbind( pollvector, polldata[!(is.na(polldata[pollutant])),][pollutant] )   
+  }
+  
+  # return the mean
+  mean(pollvector[[1]])
 }
